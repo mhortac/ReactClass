55e7dfcbaefb81af398d07263f06be491f9fbbcd
Murphy Horta <murphy@2nv.co>
2020-12-22 12:01:10 -0500

    Session #3

--- src/session1/container-component.js
+++ src/session1/container-component.js
@@ -1,28 +1,83 @@
-const mi_lista = [
-  "task_1",
-  "task_2",
-  "task_3",
-  "task_4",
-  "task_1",
-  "task_2",
-  "task_3",
-  "task_4",
-];
+import React from "react";
+
+const titleStyle = {
+  color: "red",
+  fontSize: "3em",
+};
 
 export default function Container() {
+  const [contacts, setContacts] = React.useState([]);
+
+  const [form, setForm] = React.useState({
+    name: "",
+    type: "",
+    identify: "",
+    lastName: "",
+    age: "",
+  });
+
+  function onFieldChange(event) {
+    setForm({
+      ...form,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+    console.log(form);
+    setContacts([...contacts, form]);
+  }
   return (
     <div className="layout-body">
+      <h1 style={titleStyle}> Murphy </h1>
+
+      <form className="custom-form" onSubmit={onSubmit}>
+        <label> Tipo de Identificación</label>
+        <select
+          name="type"
+          value={form.type}
+          onChange={(e) => onFieldChange(e)}
+        >
+          <option value="CC">Cédula</option>
+          <option value="T.I">Tarjeta de Identidad</option>
+          <option value="C.E">cédula de Extranjería</option>
+        </select>
+        <label> Identificación</label>
+        <input
+          name="identify"
+          value={form.identify}
+          onChange={(e) => onFieldChange(e)}
+        />
+        <label> Nombre</label>
+        <input
+          name="name"
+          value={form.name}
+          onChange={(e) => onFieldChange(e)}
+        />
+        <label> Apellido</label>
+        <input
+          name="lastName"
+          value={form.lastName}
+          onChange={(e) => onFieldChange(e)}
+        />
+        <label> Edad</label>
+        <input name="age" value={form.age} onChange={(e) => onFieldChange(e)} />
+
+        <button type="submit"> ENVIAR </button>
+      </form>
+
       <ul>
-        {mi_lista.map((item, key) => {
+        {contacts.map((item, key) => {
           return (
             <li key={key} className="item-list">
               <img
                 className="item-image"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtbfw8X_fhk4zX5l0ly9Qd9HBPZ6PCe7fEZg&usqp=CAU"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7c/User_font_awesome.svg/1200px-User_font_awesome.svg.png"
                 alt="imagen"
               />
               <div className="item-desc">
-                Este es un texto descriptivo del elemento
+                {item.name} {item.lastName}
               </div>
             </li>
           );
